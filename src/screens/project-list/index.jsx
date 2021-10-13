66d645a730353c577fac0806e004e11c4426ae7b
liutianyou <2726725268@qq.com>
2021-10-13 22:01:01 +0800

    添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useEffect, useState } from "react"
+import { SearchPanel } from "./search-panel"
+import qs from 'qs'
+import { List } from "./list"
+import { cleanObject } from '../../utils'
+const apiUrl = process.env.REACT_APP_API_URL
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [list, setList] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+      if (response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [param]);
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  }, []);
+
+  return <div>
+    <SearchPanel users={users} param={param} setParam={setParam} />
+    <List users={users} list={list} />
+  </div>
+}
---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+export const SearchPanel = ({users,param,setParam})=>{
+  return <form>
+    <div>
+      <input type='text' onChange={(e)=>{
+        setParam({
+          ...param,
+          name:e.target.value
+        })
+      }}/>
+      <select value={param.personId} onChange={(e)=>
+        setParam({
+          ...param,
+          personId:e.target.value
+        })}>
+        <option value={''}>负责人</option>
+        {
+          users.map((user)=><option key={user.id} value={user.id}>{user.name}</option>)
+        }
+      </select>
+    </div>
+  </form>
+}
